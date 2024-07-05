532b69cef6739e313dda01c24c5f1c7f656a9114
Shashi Kumar <shekhar.shashi735@gmail.com>
2024-07-05 16:48:49 +0530

    Done

--- src/pages/character/[name].jsx
+++ src/pages/character/[name].jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Box, Image, Center, Text, VStack } from '@chakra-ui/react';
+import { Box, Image, Center, Text, VStack, Button } from '@chakra-ui/react';
 import CharacterDetail from '../../views/components/CharacterDetail';
 import { fetchCharacter } from '../../views/utils/api';
+import Header from "../../views/components/Header"
 
 const CharacterPage = () => {
   const router = useRouter();
@@ -57,27 +58,47 @@ const CharacterPage = () => {
     );
   }
 
+  const handleBackClick = () => {
+    router.push('/');
+  };
+
   return (
-    <Center height="100vh">
-      <Box
-        maxW="sm"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        boxShadow="lg"
-        bg="white"
-        m={[2, 3]}
-      >
-        <VStack spacing={4}>
-          <Image
-            src={`https://starwars-visualguide.com/assets/img/characters/${character.url.match(/\/(\d+)\//)[1]}.jpg`}
-            alt={character.name}
-            borderRadius="lg"
-          />
-          <CharacterDetail character={character} />
-        </VStack>
-      </Box>
-    </Center>
+    <>
+       <Center mb={4}>
+        <Button
+          width="100px"
+          padding="10px"
+          borderRadius="5px"
+          backgroundColor="gray"
+          color="white"
+          cursor="pointer"
+          _hover={{ backgroundColor: 'white', color: 'black' }}
+          onClick={handleBackClick}
+        >
+          Back
+        </Button>
+      </Center>
+      <Center height="100vh">
+        <Box
+          maxW="sm"
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          boxShadow="lg"
+          bg="white"
+          m={[2, 3]}
+        >
+          <VStack spacing={4}>
+            <Image
+              src={`https://starwars-visualguide.com/assets/img/characters/${character.url.match(/\/(\d+)\//)[1]}.jpg`}
+              alt={character.name}
+              borderRadius="lg"
+            />
+            <CharacterDetail character={character} />
+          </VStack>
+        </Box>
+      </Center>
+    </>
   );
 };
 
